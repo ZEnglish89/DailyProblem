4d8fba35d05193a2592f89b95707acd2c1f7ce99
Zach English <zenglish555@gmail.com>
2024-02-22 14:39:10 -0800

    Optimal solution with explanation

--- ProductArray.py
+++ ProductArray.py
@@ -36,9 +36,25 @@ def NoDiv(list):
         newlist.append(product)
     return newlist
 
+#This is the result of me looking at solutions on Leetcode lol
+def NoDivCorrect(list):
+    length=len(list)
+    ans = [1]*len(list)
+    suf=1 #represents the product of all values to the "right" of the current one
+    pre=1 #the product of all values to the "left"
+    for i in range(length):
+        ans[i]*=pre #multiply the current value(sweeping from left to right) by the product to its left
+        pre*=list[i] #update pre to include one more value to the right
+        ans[length-1-i]*=suf #multiply the current value(sweeping from right to left) by the product on its right
+        suf*=list[length-1-i] #update suf to include one more value to the left
+#By the end of the loop, every value will have been multiplied by the products on both its left and right, that is
+#they will have been multiplied by both suf and pre, but not themselves, achieving the correct result.
+    return ans
 a=[1,2,3,4,5]
 b=[3,2,1]
 print(Easy(a))
 print(Easy(b))
 print(NoDiv(a))
 print(NoDiv(b))
+print(NoDivCorrect(a))
+print(NoDivCorrect(b))
